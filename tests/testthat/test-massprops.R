31a26af136dd3df7bf50bb9fd899b6c27a5079d5
Steven Jenkins <j.s.jenkins@jpl.nasa.gov>
2025-01-08 08:55:14 -0500

    more tests

--- tests/testthat/test-massprops.R
+++ tests/testthat/test-massprops.R
@@ -12,7 +12,8 @@ mp_set <- list(
     σ_inertia = matrix(data = c(0.372229441260767,  0.0219147131886038, 0.0214698151227243,
                                 0.0219147131886038, 0.36942277459594,   0.0224252422361279,
                                 0.0214698151227243, 0.0224252422361279, 0.374164784864648), nrow = 3, byrow = 3, dimnames = list(xyz, xyz)
-                       )
+                       ),
+    point = FALSE
   ),
   '-' = list(
     mass = 0.341313654366182,
@@ -26,7 +27,8 @@ mp_set <- list(
     σ_inertia = matrix(data = c(0.372229441260767,  0.0219147131886038, 0.0214698151227243,
                                 0.0219147131886038, 0.36942277459594,   0.0224252422361279,
                                 0.0214698151227243, 0.0224252422361279, 0.374164784864648), nrow = 3, byrow = 3, dimnames = list(xyz, xyz)
-    )
+                      ),
+    point = FALSE
   )
 )
 
@@ -36,7 +38,7 @@ test_that("df_get_mass_props() works for positive convention", {
   expect_equal(result$mass, mp_set$"+"$mass)
   expect_equal(result$center_mass, mp_set$"+"$center_mass)
   expect_equal(result$inertia, mp_set$"+"$inertia)
-  expect_false(result$point)
+  expect_equal(result$point, mp_set$"+"$point)
 })
 
 test_that("df_get_mass_props() works for negative convention", {
@@ -45,7 +47,7 @@ test_that("df_get_mass_props() works for negative convention", {
   expect_equal(result$mass, mp_set$"-"$mass)
   expect_equal(result$center_mass, mp_set$"-"$center_mass)
   expect_equal(result$inertia, mp_set$"-"$inertia)
-  expect_false(result$point)
+  expect_equal(result$point, mp_set$"+"$point)
 })
 
 test_that("df_get_mass_props_and_unc() works for positive convention", {
@@ -54,7 +56,7 @@ test_that("df_get_mass_props_and_unc() works for positive convention", {
   expect_equal(result$mass, mp_set$"+"$mass)
   expect_equal(result$center_mass, mp_set$"+"$center_mass)
   expect_equal(result$inertia, mp_set$"+"$inertia)
-  expect_false(result$point)
+  expect_equal(result$point, mp_set$"+"$point)
   expect_equal(result$σ_mass, mp_set$"+"$σ_mass)
   expect_equal(result$σ_center_mass, mp_set$"+"$σ_center_mass)
   expect_equal(result$σ_inertia, mp_set$"+"$σ_inertia)
@@ -66,9 +68,50 @@ test_that("df_get_mass_props_and_unc() works for negative convention", {
   expect_equal(result$mass, mp_set$"-"$mass)
   expect_equal(result$center_mass, mp_set$"-"$center_mass)
   expect_equal(result$inertia, mp_set$"-"$inertia)
-  expect_false(result$point)
+  expect_equal(result$point, mp_set$"+"$point)
   expect_equal(result$σ_mass, mp_set$"-"$σ_mass)
   expect_equal(result$σ_center_mass, mp_set$"-"$σ_center_mass)
   expect_equal(result$σ_inertia, mp_set$"-"$σ_inertia)
 })
 
+test_that("df_set_mass_props() works for positive convention", {
+  input <- mp_set$"+"
+  input$poi_conv = "+"
+  df <- data.frame(id = c("C.1.2.2.3.1.2.3", "C.1.2.2.3.2.1.1"))
+  result <- df_set_mass_props(df, "C.1.2.2.3.1.2.3", input)[1, ]
+
+  expect_equal(result$mass, input$mass)
+  expect_equal(result$Cx, input$center_mass[["x"]])
+  expect_equal(result$Cy, input$center_mass[["y"]])
+  expect_equal(result$Cz, input$center_mass[["z"]])
+  it <- input$inertia
+  expect_equal(result$Ixx, it["x", "x"])
+  expect_equal(result$Iyy, it["y", "y"])
+  expect_equal(result$Izz, it["z", "z"])
+  expect_equal(result$Ixy, -it["x", "y"])
+  expect_equal(result$Ixz, -it["x", "z"])
+  expect_equal(result$Iyz, -it["y", "z"])
+  expect_equal(result$POIconv, input$poi_conv)
+  expect_equal(result$Ipoint, input$point)
+})
+
+test_that("df_set_mass_props() works for negative convention", {
+  input <- mp_set$"-"
+  input$poi_conv = "-"
+  df <- data.frame(id = c("C.1.2.2.3.1.2.3", "C.1.2.2.3.2.1.1"))
+  result <- df_set_mass_props(df, "C.1.2.2.3.2.1.1", input)[2, ]
+
+  expect_equal(result$mass, input$mass)
+  expect_equal(result$Cx, input$center_mass[["x"]])
+  expect_equal(result$Cy, input$center_mass[["y"]])
+  expect_equal(result$Cz, input$center_mass[["z"]])
+  it <- input$inertia
+  expect_equal(result$Ixx, it["x", "x"])
+  expect_equal(result$Iyy, it["y", "y"])
+  expect_equal(result$Izz, it["z", "z"])
+  expect_equal(result$Ixy, it["x", "y"])
+  expect_equal(result$Ixz, it["x", "z"])
+  expect_equal(result$Iyz, it["y", "z"])
+  expect_equal(result$POIconv, input$poi_conv)
+  expect_equal(result$Ipoint, input$point)
+})
