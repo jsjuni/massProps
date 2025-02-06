baed94b4d7e0bb455f569e232814c6d33f72befb
James Steven Jenkins <sjenkins@studioj.us>
2025-02-06 11:41:49 -0500

    tests updated for 100% coverage

--- tests/testthat/test-radii.R
+++ tests/testthat/test-radii.R
@@ -8,6 +8,16 @@ test_that("add_radii_of_gyration() works", {
   expect_true(all.equal(out_df, in_df))
 })
 
+test_that("add_radii_of_gyration_unc() works", {
+  in_df <- add_radii_of_gyration(sawe_table)
+  out_df <- add_radii_of_gyration_unc(in_df)
+  in_df$sigma_kx <- sqrt(in_df$sigma_Ixx^2 / (in_df$mass * in_df$Ixx) + (in_df$Ixx * in_df$sigma_mass^2) / in_df$mass^3) / 2
+  in_df$sigma_ky <- sqrt(in_df$sigma_Iyy^2 / (in_df$mass * in_df$Iyy) + (in_df$Iyy * in_df$sigma_mass^2) / in_df$mass^3) / 2
+  in_df$sigma_kz <- sqrt(in_df$sigma_Izz^2 / (in_df$mass * in_df$Izz) + (in_df$Izz * in_df$sigma_mass^2) / in_df$mass^3) / 2
+
+  expect_true(all.equal(out_df, in_df))
+})
+
 test_that("get_mass_props_and_unc_and_radii() works", {
   in_df <- mp_table[which(!is.na(mp_table$mass))[1:20], ]
   out_df <- add_radii_of_gyration(in_df)
@@ -18,6 +28,21 @@ test_that("get_mass_props_and_unc_and_radii() works", {
   expect_equal(unname(actual$radii_gyration["x"]), unname(expected$kx))
   expect_equal(unname(actual$radii_gyration["y"]), unname(expected$ky))
   expect_equal(unname(actual$radii_gyration["z"]), unname(expected$kz))
+})
+
+test_that("get_mass_props_and_unc_and_radii_and_unc() works", {
+  in_df <- add_radii_of_gyration(mp_table[which(!is.na(mp_table$mass))[1:20], ])
+  out_df <- add_radii_of_gyration_unc(in_df)
+  expected <- out_df[which(out_df$id == "C.1.1.1.1.1.1.1"), ]
+  actual <- get_mass_props_and_unc_and_radii_and_unc(out_df, "C.1.1.1.1.1.1.1")
+
+  expect_equal(unname(actual$mass), unname(expected$mass))
+  expect_equal(unname(actual$radii_gyration["x"]), unname(expected$kx))
+  expect_equal(unname(actual$radii_gyration["y"]), unname(expected$ky))
+  expect_equal(unname(actual$radii_gyration["z"]), unname(expected$kz))
+  expect_equal(unname(actual$sigma_radii_gyration["x"]), unname(expected$sigma_kx))
+  expect_equal(unname(actual$sigma_radii_gyration["y"]), unname(expected$sigma_ky))
+  expect_equal(unname(actual$sigma_radii_gyration["z"]), unname(expected$sigma_kz))
 })
 
 test_that("set_radii_of_gyration_unc() works", {
